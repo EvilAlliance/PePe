4438bd32252468aed6dfe3dd9c9c3fefd5cf6ce9
EvilAlliance <chialanza17@gmail.com>
2024-09-04 12:17:32 -0300

    Hopefully last try

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 if [[ -z $1 ]] && [[ $1 -eq "replay" ]]; then
-    python3 ./test/rere.py replay ./test/test.list
+    ./test/rere.py replay ./test/test.list
 elif [[ -z $1 ]] && [[ $1 -eq "record" ]]; then
-    python3 ./test/rere.py record ./test/test.list
+    ./test/rere.py record ./test/test.list
 else
-    python3 ./test/rere.py replay ./test/test.list
+    ./test/rere.py replay ./test/test.list
 fi
---
+++ test/test.list.bi
@@ -0,0 +1,8 @@
+:i count 1
+:b shell 9
+zig build
+:i returncode 0
+:b stdout 0
+
+:b stderr 0
+
